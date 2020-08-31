433bc70a24a222e4d0a2fc0e3f96497ddcbb75a3
Emma Li <emma.m.li.98@gmail.com>
2020-08-31 12:36:15 -0700

    Assignment Complete

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-// TODO
+var GroceryList = (props) => (
+  <ul>
+    {props.items.map(item =>
+      <GroceryListItem item={item} />
+      )}
+  </ul>
+);
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemHover() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+    var style = {
+      fontWeight: this.state.done ? 'bold' : 'normal'
+    };
+
+    return (
+      <li style={style} onMouseEnter={this.onListItemHover.bind(this)} onMouseLeave={this.onListItemHover.bind(this)}>{this.props.item}</li>
+    )
+  }
+}
+
+ReactDOM.render(<GroceryList items={['White Claw', 'Haagen Dazs', 'Andes mints']}/>, document.getElementById("app"));
